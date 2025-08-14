ae42b582709c0a5572e2368396a4e00bdd857e29
brian h <git@qovert.com>
2025-08-14 08:35:45 -0700

    initial commit

---
+++ cmd/collect.go
@@ -0,0 +1,40 @@
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+// collectCmd represents the collect command
+var collectCmd = &cobra.Command{
+	Use:   "collect",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("collect called")
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(collectCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// collectCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// collectCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
